7d9e7b3fea8cceb2a78910ddbc4cc47494c7a51e
Tsymbaliuk-Oleksandr <103380042+Tsymbaliuk-Oleksandr@users.noreply.github.com>
2022-08-09 10:29:25 +0000

    Deploying to gh-pages from @ Tsymbaliuk-Oleksandr/Team-project@82db60faa0b46ce64bbfa71739f3d1dbae5cdbe9 🚀

---
+++ index.48b81258.js
@@ -0,0 +1,2 @@
+new Swiper(".slider",{pagination:{el:".swiper-pagination",clickable:!0},simulateTouch:!0,grabCursor:!0,loop:!0,autoplay:{dalay:750,disableOnInteraction:!1},speed:800});
+//# sourceMappingURL=index.48b81258.js.map
---
+++ index.54d8bc12.js
@@ -0,0 +1,2 @@
+new Swiper(".slider",{pagination:{el:".swiper-pagination",clickable:!0},simulateTouch:!0,grabCursor:!0,loop:!0,autoplay:{dalay:750,disableOnInteraction:!1},speed:800});
+//# sourceMappingURL=index.54d8bc12.js.map
